fc5487b6daf4a3650e4d7a81b574baac3b0195f5
dquangucsd <dquang@ucsd.edu>
2022-02-04 01:18:39 -0800

    First commit-added tableview with movies
    First save point - added scrollable table view with list of movies

---
+++ Flixster/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Flixster
+//
+//  Created by Danny Quang on 2/4/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
